704dc5fad208c42e3b1c312c071ae74453b56bed
Ancor Gonzalez Sosa <ancor@suse.de>
2013-04-11 15:34:32 +0200

    Hacky but working js_link fix

--- app/assets/javascripts/filters/js_link.js
+++ app/assets/javascripts/filters/js_link.js
@@ -2,6 +2,6 @@ app.filter("js_link", function() {
   return function(input) {
     if (_.isUndefined(input) || _.isNull(input)) return "";
     
-    return input.replace(new RegExp('\\[([^\\]]+)\\]\\(([^\\)]+)\\)', 'mg'), '<a data-remote="true" href="$2">$1</a>')
+    return input.replace(new RegExp('\\[([^\\]]+)\\]\\(([^\\)]+)\\)', 'mg'), '<a onClick="$.ajax({url: \'$2\'})" href="#">$1</a>')
   };
 });
